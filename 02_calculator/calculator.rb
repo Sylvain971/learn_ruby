6135ef23c4f5e1c99520c60a64536d15fec3d6d0
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-01-16 16:53:17 +0100

    ajout de calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add (num1, num2)
+	sum = num1 + num2
+end
+
+def subtract (num1, num2)
+	num = num1 - num2
+end
+
+def sum (array)
+		asum = 0
+		array.each { |number| asum += number }
+		asum
+end
