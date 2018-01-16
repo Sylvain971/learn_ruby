d80e4d16c154a8413e15fd691d6b5160e9284b35
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-01-16 15:44:38 +0100

    Hello et temperature complets

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,17 @@
 #write your code here
+=begin one_farenheit_in_celsius = (5/9).to_f
+one_farenheit = 1.to_f
+=end
+
+
+def ftoc(temperature)
+	#"On entre dans la fonction ftoc"
+	celsius = ((temperature - 32) / 1.8).round(2)
+
+end
+
+def ctof(temperature)
+	#{}"On entre dans la fonction ctof"
+	farheneit = ((temperature * 1.8) + 32).round(2)
+
+end
